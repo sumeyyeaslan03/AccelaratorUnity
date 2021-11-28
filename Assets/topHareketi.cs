946930c6a04fdc8dfa817e3d0648f3c7d7a49a5b
sumey <sumeyye72.sa@gmail.com>
2021-11-28 21:25:11 +0300

    AccelaratorUnity

---
+++ Assets/topHareketi.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class topHareketi : MonoBehaviour
+{
+    Rigidbody2D rgb;
+    //speed for ball
+    public float hiz;
+    float posX, posY;
+
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        posX = Input.acceleration.x * hiz;
+        posY = Input.acceleration.y * hiz;
+
+    }
+    void FixedUpdate(){
+        rgb.velocity= new Vector2(rgb.velocity.x + posX, rgb.velocity.y + posY);
+    }
+}
